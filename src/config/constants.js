61efe28fdc2d99617259072b5985b95ae31cf53c
JefferDevop <77632174+JefferDevop@users.noreply.github.com>
2026-06-02 04:16:25 -0500

    Update BASE_API URL for the application

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API = 'https://majos.conexion.bar';
+export const BASE_API = 'https://majos.cosmoqa.store';
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
